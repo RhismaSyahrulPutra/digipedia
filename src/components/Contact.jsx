e643e59f8e7f54c1a73845614f7a28553caef69f
Choiyoungdok <rhismasyahrulputra11@gmail.com>
2025-01-21 01:18:05 +0700

    update ui contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,10 +2,103 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div id="contact" className="pt-16 bg-gray-100 min-h-screen">
-      <h1 className="text-center text-3xl font-semibold text-gray-800">
-        Contact
-      </h1>
+    <div id="contact" className="pt-16 bg-gray-100 min-h-screen px-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 mt-8 px-6">
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+          <h3 className="text-2xl font-semibold text-center">Contact Us</h3>
+          <div className="mt-4">
+            <form>
+              <div className="mb-4">
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  placeholder="John"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="lastName"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  placeholder="Doe"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  placeholder="john.doe@example.com"
+                />
+              </div>
+              <div className="mb-4">
+                <label
+                  htmlFor="message"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  className="mt-1 p-2 w-full border border-gray-300 rounded-md"
+                  placeholder="Your message here..."
+                />
+              </div>
+              <button
+                type="submit"
+                className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-200"
+              >
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
+
+        {/* Card 2 - Our Office and Get in Touch */}
+        <div className="bg-white p-6 rounded-lg shadow-lg">
+          <h3 className="text-2xl font-semibold text-center">Our Office</h3>
+          <div className="mt-4">
+            {/* Google Map embedded */}
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126748.60965089516!2d107.56075449198627!3d-6.903271950805587!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e6398252477f%3A0x146a1f93d3e815b2!2sBandung%2C%20Kota%20Bandung%2C%20Jawa%20Barat!5e0!3m2!1sid!2sid!4v1737396960766!5m2!1sid!2sid"
+              width="100%"
+              height="300"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
+
+          {/* Get in Touch */}
+          <h3 className="text-2xl font-semibold text-center mt-8">
+            Get in Touch
+          </h3>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Pricing = () => {
   return (
-    <div id="pricing" className="pt-16 bg-gray-100 min-h-screen">
+    <div id="pricing" className="pt-24 bg-gray-100 min-h-screen">
       <h1 className="text-center text-3xl font-semibold text-gray-800">
         Pricing
       </h1>
